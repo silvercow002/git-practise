39cc84da8ea1c2ec9594339ec0a0427addf3fc21
Mukuiiiii <justinweng0423@gmail.com>
2026-04-22 14:28:55 +0800

    first commit

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,7 +2,7 @@
 public class FirstClass4Team {
     public static void main(String[] atgs) {
         System.out.println("the code for practise git operate.");
-        System.out.printf("學號\"%d\"%n姓名:\"%s\"%n", 01357012, "黃翊銘");
-
+        System.out.printf("學號\"%s\"%n姓名:\"%s\"%n", "01357012", "黃翊銘");
+        System.out.printf("學號\"%s\"%n姓名:\"%s\"%n", "01357009", "翁席竑");
     }
 }
