71ba6c18e40abc4bc47dd5cc9c17ce552f1f202a
silv <wsl@silvercow.zip>
2026-04-22 14:17:34 +0800

    leader

---
+++ FirstClass4Team.java
@@ -0,0 +1,8 @@
+
+public class FirstClass4Team {
+    public static void main(String[] atgs) {
+        System.out.println("the code for practise git operate.");
+        System.out.printf("學號\"%d\"%n姓名:\"%s\"%n", 01357012, "黃翊銘");
+
+    }
+}
