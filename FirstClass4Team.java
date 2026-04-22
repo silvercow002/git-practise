c36f339de717a6947c58fc4ab31afe41a628ddc8
stone <107040george@gmail.com>
2026-04-22 14:36:42 +0800

    我的學號跟名字

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,6 +3,6 @@ public class FirstClass4Team {
     public static void main(String[] atgs) {
         System.out.println("the code for practise git operate.");
         System.out.printf("學號\"%s\"%n姓名:\"%s\"%n", "01357012", "黃翊銘");
-        System.out.printf("學號\"%s\"%n姓名:\"%s\"%n", "01357009", "翁席竑");
-    }
+        System.out.printf("學號\"%s\"%n姓名:\"%s\"%n", "01357009", "翁席竑"
+   }
 }
